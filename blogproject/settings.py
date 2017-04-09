446039b0687e5e0a531f1be92ade86cebdf2bb62
ttbug <ttbuge@gmail.com>
2017-04-09 08:50:20 +0800

    fix some issue

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -136,7 +136,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'exampledb',
         'USER': 'dbuser',
-        'PASSWORD': '5513934223',
+        'PASSWORD': '******',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
